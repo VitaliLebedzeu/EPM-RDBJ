59de014822027fa2597c273a3510825263db03a5
Vitali Lebedzeu <Vitali_Lebedzeu@epam.com>
2020-11-28 16:00:49 +0300

    add suppress warnings annotation

--- src/main/java/task1/model/vehicle/air/AirVehicleType.java
+++ src/main/java/task1/model/vehicle/air/AirVehicleType.java
@@ -1,5 +1,11 @@
 package task1.model.vehicle.air;
 
 public enum AirVehicleType {
-    PLANE, HELICOPTER, QUADCOPTER, DRONE
+    PLANE, HELICOPTER, QUADCOPTER,
+
+    /**
+     * This is not popular vehicle and not recommended for using
+     */
+    @SuppressWarnings("unused")
+    DRONE
 }
