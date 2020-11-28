fe731c35f6ae3b2588bb70da8d4b839588d993e3
Vitali Lebedzeu <Vitali_Lebedzeu@epam.com>
2020-11-29 01:04:51 +0300

    add reflection methods for invoking class methods

--- src/main/java/ReflectionApi.java
+++ src/main/java/ReflectionApi.java
@@ -5,26 +5,37 @@ import task1.model.vehicle.air.MilitaryAirVehicleType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class ReflectionApi {
 
-    private static final String FIELD_NOT_EXIST_MESSAGE_FORMAT = "The '%s' class does not contain '%s' field.\n";
+    private static final String FIELD_NOT_EXIST_MESSAGE_FORMAT = "The '%s' class does not have '%s' field.\n";
     private static final String CLASS_NOT_EXIST_MESSAGE_FORMAT = "The '%s' class is not exist.\n";
     private static final String CLASS_NOT_HAS_CONSTRUCTOR_MESSAGE_FORMAT = "The '%s' class does not have the '%s' constructor.\n";
+    private static final String METHOD_NOT_EXIST_MESSAGE_FORMAT = "The '%s' class does not have '%s' method.\n";
 
-    public static Object createAirVehicle(String className) {
+    public static Object createClass(String className) {
+        return createClass(className, null);
+    }
+
+    public static <R> Object createClass(String className, R r) {
         Class<?> clazz = getClass(className);
-        Constructor<?> constructor = getClassConstructor(clazz);
-        return createInstance(constructor);
+        if (r == null) {
+            Constructor<?> constructor = getClassConstructor(clazz);
+            return createInstance(constructor);
+        } else {
+            Constructor<?> constructor = getClassConstructor(clazz, r.getClass());
+            return createInstance(constructor, r);
+        }
     }
 
     public static <C> void setAllClassFields(C c, Class<?> clazz) {
         Field[] fields = clazz.getFields();
-        fillFields(c, fields);
+        setAllClassFields(c, fields);
 
         Field[] declaredFields = clazz.getDeclaredFields();
-        fillFields(c, declaredFields);
+        setAllClassFields(c, declaredFields);
 
         Class<?> superClass = clazz.getSuperclass();
         if (superClass.getFields().length != 0 || superClass.getDeclaredFields().length != 0) {
@@ -32,24 +43,60 @@ public class ReflectionApi {
         }
     }
 
-    public static <C> void setClassField(C c, Class<?> clazz, String fieldName, Object... values) {
-        Field field = null;
-        try {
-            field = clazz.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
-            System.err.printf(FIELD_NOT_EXIST_MESSAGE_FORMAT, clazz.getSimpleName(), e.getMessage());
-        }
+    public static <C> void setClassField(C c, Class<?> clazz, String fieldName, Object value) {
+        Field field = getField(clazz, fieldName);
         if (field == null) {
             Class<?> superClass = clazz.getSuperclass();
             if (superClass.getFields().length != 0 | superClass.getDeclaredFields().length != 0) {
-                setClassField(c, superClass, fieldName, values);
+                setClassField(c, superClass, fieldName, value);
             } else {
                 System.err.printf(FIELD_NOT_EXIST_MESSAGE_FORMAT, clazz.getSimpleName(), new NoSuchFieldException().getMessage());
             }
         } else {
             field.setAccessible(true);
-            setField(c, field, values);
+            setField(c, field, value);
         }
+    }
+
+    @SafeVarargs
+    public static <C, V> void invokeVoidClassMethod(C c, String methodName, V... values) {
+        Method method = null;
+        try {
+            method = (values == null) ? c.getClass().getMethod(methodName) : c.getClass().getMethod(methodName, values.getClass());
+        } catch (NoSuchMethodException e) {
+            System.err.printf(METHOD_NOT_EXIST_MESSAGE_FORMAT, c.getClass().getSimpleName(), e.getMessage());
+        }
+        if (method != null) {
+            try {
+                if (values == null) {
+                    method.invoke(c);
+                } else {
+                    method.invoke(c, (Object) values);
+                }
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <C, T> T invokeClassMethod(C c, String methodName) {
+        Method method = null;
+        try {
+            method = c.getClass().getMethod(methodName);
+        } catch (NoSuchMethodException e) {
+            System.err.printf(METHOD_NOT_EXIST_MESSAGE_FORMAT, c.getClass().getSimpleName(), e.getMessage());
+        }
+        T t = null;
+        if (method != null) {
+            try {
+                t = (T) method.invoke(c);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                System.err.println(Arrays.toString(e.getStackTrace()));
+            }
+
+        }
+        return t;
     }
 
     private static Class<?> getClass(String className) {
@@ -62,65 +109,97 @@ public class ReflectionApi {
     }
 
     private static Constructor<?> getClassConstructor(Class<?> clazz) {
+        return getClassConstructor(clazz, null);
+    }
+
+    private static Constructor<?> getClassConstructor(Class<?> clazz, Class<?> constructorClass) {
+        Constructor<?> constructor = null;
         if (clazz == null) {
             System.err.printf(CLASS_NOT_EXIST_MESSAGE_FORMAT, new ClassNotFoundException().getMessage());
         } else {
             try {
-                return clazz.getConstructor();
+                constructor = (constructorClass == null) ? clazz.getConstructor() : clazz.getConstructor(constructorClass);
             } catch (NoSuchMethodException e) {
                 System.err.printf(CLASS_NOT_HAS_CONSTRUCTOR_MESSAGE_FORMAT, clazz.getSimpleName(), e.getMessage());
             }
         }
-        return null;
+        return constructor;
     }
 
     private static Object createInstance(Constructor<?> constructor) {
+        return createInstance(constructor, (Object) null);
+    }
+
+    private static <V> Object createInstance(Constructor<?> constructor, V v) {
+        Object object = null;
         if (constructor == null) {
             System.err.printf("The '%s' constructor is not exist.\n", new NoSuchMethodException().getMessage());
         } else {
             try {
-                return constructor.newInstance();
+                object = (v == null) ? constructor.newInstance() : constructor.newInstance(v);
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
                 System.err.println(Arrays.toString(e.getStackTrace()));
             }
         }
-        return null;
+        return object;
     }
 
-    private static <C> void setField(C c, Field field, Object... values) {
-        Arrays.stream(values).forEach(value -> {
+    private static Field getField(Class<?> clazz, String fieldName) {
+        Field field = null;
+        try {
+            field = clazz.getField(fieldName);
+        } catch (NoSuchFieldException e) {
+            System.err.printf(FIELD_NOT_EXIST_MESSAGE_FORMAT, clazz.getSimpleName(), e.getMessage());
+        }
+        if (field == null) {
             try {
-                field.set(c, value);
-            } catch (IllegalAccessException e) {
-                System.err.println(Arrays.toString(e.getStackTrace()));
+                field = clazz.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+                System.err.printf(FIELD_NOT_EXIST_MESSAGE_FORMAT, clazz.getSimpleName(), e.getMessage());
             }
-        });
+        }
+        return field;
     }
 
-    private static <C> void fillFields(C c, Field[] fields) {
+    private static <C> void setField(C c, Field field, Object value) {
+        try {
+            field.set(c, value);
+        } catch (IllegalAccessException e) {
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    private static <C> void setAllClassFields(C c, Field[] fields) {
         Arrays.stream(fields).forEach(f -> {
             f.setAccessible(true);
             String fieldName = f.getType().getSimpleName();
             switch (fieldName) {
-                case "String": setField(c, f, RandomStringUtils.randomAlphabetic(6));
+                case "String":
+                    setField(c, f, RandomStringUtils.randomAlphabetic(6));
                     break;
 
-                case "double": setField(c, f, Math.random() * 100);
+                case "double":
+                    setField(c, f, Math.random() * 100);
                     break;
 
-                case "int": setField(c, f, (int) (Math.random() * 1000));
+                case "int":
+                    setField(c, f, (int) (Math.random() * 1000));
                     break;
 
-                case "boolean": setField(c, f, true);
+                case "boolean":
+                    setField(c, f, true);
                     break;
 
-                case "MilitaryAirVehicleType": setField(c, f, MilitaryAirVehicleType.FIGHTER);
+                case "MilitaryAirVehicleType":
+                    setField(c, f, MilitaryAirVehicleType.FIGHTER);
                     break;
 
-                case "AirVehicleType": setField(c, f, AirVehicleType.PLANE);
+                case "AirVehicleType":
+                    setField(c, f, AirVehicleType.PLANE);
                     break;
 
-                default: setField(c, f, (Object) null);
+                default:
+                    setField(c, f, null);
             }
         });
     }
--- src/main/java/ReflectionMain.java
+++ src/main/java/ReflectionMain.java
@@ -1,31 +1,35 @@
 import task1.model.AirCompany;
+import task1.model.vehicle.air.AirVehicleType;
 import task1.model.vehicle.air.CivilAirVehicle;
 import task1.model.vehicle.air.MilitaryAirVehicle;
 
 public class ReflectionMain {
 
     public static void main(String[] args) {
-        CivilAirVehicle airbus320 = (CivilAirVehicle) ReflectionApi.createAirVehicle("task1.model.vehicle.air.CivilAirVehicle");
-        CivilAirVehicle twinRanger = (CivilAirVehicle) ReflectionApi.createAirVehicle("task1.model.vehicle.air.CivilAirVehicle");
-        MilitaryAirVehicle phantom = (MilitaryAirVehicle) ReflectionApi.createAirVehicle("task1.model.vehicle.air.MilitaryAirVehicle");
-        MilitaryAirVehicle falcon = (MilitaryAirVehicle) ReflectionApi.createAirVehicle("task1.model.vehicle.air.MilitaryAirVehicle");
-        MilitaryAirVehicle hind = (MilitaryAirVehicle) ReflectionApi.createAirVehicle("task1.model.vehicle.air.MilitaryAirVehicle");
-        MilitaryAirVehicle quad = (MilitaryAirVehicle) ReflectionApi.createAirVehicle("task1.model.vehicle.air.MilitaryAirVehicle");
+        CivilAirVehicle airbus320 = (CivilAirVehicle) ReflectionApi.createClass("task1.model.vehicle.air.CivilAirVehicle");
+        CivilAirVehicle twinRanger = (CivilAirVehicle) ReflectionApi.createClass("task1.model.vehicle.air.CivilAirVehicle");
+        MilitaryAirVehicle phantom = (MilitaryAirVehicle) ReflectionApi.createClass("task1.model.vehicle.air.MilitaryAirVehicle");
+        MilitaryAirVehicle falcon = (MilitaryAirVehicle) ReflectionApi.createClass("task1.model.vehicle.air.MilitaryAirVehicle");
+        MilitaryAirVehicle hind = (MilitaryAirVehicle) ReflectionApi.createClass("task1.model.vehicle.air.MilitaryAirVehicle");
+        MilitaryAirVehicle quad = (MilitaryAirVehicle) ReflectionApi.createClass("task1.model.vehicle.air.MilitaryAirVehicle");
 
-        ReflectionApi.setAllClassFields(airbus320, airbus320.getClass());
+        ReflectionApi.setClassField(airbus320, airbus320.getClass(), "airVehicleType", AirVehicleType.PLANE);
+        ReflectionApi.setClassField(airbus320, airbus320.getClass(), "name", "Airbus A320");
+        ReflectionApi.setClassField(airbus320, airbus320.getClass(), "range", 3300);
+        ReflectionApi.setClassField(airbus320, airbus320.getClass(), "emptyWeight", 37.23);
+        ReflectionApi.setClassField(airbus320, airbus320.getClass(), "maxTakeoffWeight", 57.13);
+        ReflectionApi.setClassField(airbus320, airbus320.getClass(), "capacity", 180);
+
         ReflectionApi.setAllClassFields(twinRanger, airbus320.getClass());
         ReflectionApi.setAllClassFields(phantom, phantom.getClass());
         ReflectionApi.setAllClassFields(falcon, falcon.getClass());
         ReflectionApi.setAllClassFields(hind, hind.getClass());
         ReflectionApi.setAllClassFields(quad, quad.getClass());
 
-        AirCompany aeroflot = (AirCompany) ReflectionApi.createAirVehicle("task1.model.AirCompany");
-        ReflectionApi.setClassField(aeroflot, aeroflot.getClass(), "name", "Aeroflot");
-        ReflectionApi.setClassField(aeroflot, aeroflot.getClass(), "airVehicles", airbus320, twinRanger, phantom, falcon, hind, quad);
-
-        System.out.println(falcon.getInfo());
-        System.out.println(airbus320.getInfo());
-        System.out.println(phantom.getInfo());
+        AirCompany aeroflot = (AirCompany) ReflectionApi.createClass("task1.model.AirCompany", "Aeroflot");
+        ReflectionApi.invokeVoidClassMethod(aeroflot, "addAirVehicle", airbus320, twinRanger, phantom, falcon, hind, quad);
+        
+        System.out.println((String) ReflectionApi.invokeClassMethod(aeroflot, "getAirVehiclesInfo"));
 
     }
 }
